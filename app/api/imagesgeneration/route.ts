a5f570defbae0ab517457cc2790c9f0e5f031858
Nainish Rai <nainishraisjp@gmail.com>
2023-09-08 22:09:02 +0530

    replaced axios on server side

--- app/api/imagesgeneration/route.ts
+++ app/api/imagesgeneration/route.ts
@@ -4,17 +4,30 @@ export const POST = async (req: NextRequest) => {
   try {
     const reqBody = await req.json();
 
-    const response = await axios.post(
+    // const response = await axios.post(
+    //   `${process.env.API_BASE!}/images/generations`,
+    //   reqBody,
+    //   {
+    //     headers: {
+    //       Authorization: `Bearer ${process.env.API_KEY!}`,
+    //     },
+    //   }
+    // );
+    const rawResponse = await fetch(
       `${process.env.API_BASE!}/images/generations`,
-      reqBody,
       {
+        method: "POST",
         headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
           Authorization: `Bearer ${process.env.API_KEY!}`,
         },
+        body: JSON.stringify(reqBody),
       }
     );
+    const content = await rawResponse.json();
 
-    return NextResponse.json(response.data);
+    return NextResponse.json(content);
   } catch (error) {
     return NextResponse.error();
   }
