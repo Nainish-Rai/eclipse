b0afbb0e9a96bb286361967c2c217953a87cf902
Nainish Rai <nainishraisjp@gmail.com>
2023-09-08 13:02:57 +0530

    fix2

--- app/api/auth/callback/google/route.ts
+++ app/api/auth/callback/google/route.ts
@@ -1,9 +1,4 @@
-import { redirect } from "next/dist/server/api-utils";
 import { NextRequest, NextResponse } from "next/server";
 export const GET = async (req: NextRequest) => {
-  try {
-    return NextResponse.redirect(new URL("/studio", req.url));
-  } catch (error) {
-    return NextResponse.error();
-  }
+  return NextResponse.redirect(new URL("/studio", req.url));
 };
